41db1139ff3b77b2f4b99206f1facdbc04645778
Wantul <irwndandka@gmail.com>
2022-06-28 00:36:44 +0700

    create, read, update, and delete

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,31 @@
-const routes = [];
+const { addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler } = require('./handler')
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler
+    },
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getBookByIdHandler
+    },
+    {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: editBookByIdHandler
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookByIdHandler
+    }
+];
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const server = Hapi.server({
     port: 5000,
     host: 'localhost',
+    routes: {
+        cors: {
+            origin: ['*']
+        }
+    }
 });
 
 const init = async () => {
+    server.route(routes);
     await server.start();
     console.log(`Server berjalan di ${server.info.uri}`)
 }
